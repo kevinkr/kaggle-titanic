995858cf6679b5e74a38af5f12901e612ab1b5d2
kevinkr <kevinkr@yahoo.com>
2016-07-07 13:37:22 -0500

    Minor mods for better package mgmt.

--- Development/wherley-titanic.R
+++ Development/wherley-titanic.R
@@ -31,8 +31,18 @@ test.raw <- readData(Titanic.path, test.data.file,
                      test.column.types, missing.types)
 df.infer <- test.raw
 
+require(plyr)     # for the revalue function 
+require(stringr)  # for the str_sub function
+require(Amelia)   # For missing data mssmap
+require(Hmisc)    # for impute and some bystats
+require(vcd)
+require(corrgram)
+require(caret)
+require(pROC)
+require(ada)
+
+
 ## map missing data by provided feature
-require(Amelia)
 missmap(df.train, main="Titanic Training Data - Missings Map", 
         col=c("yellow", "black"), legend=FALSE)
 
@@ -56,7 +66,6 @@ barplot(table(df.train$Embarked),
         names.arg = c("Cherbourg", "Queenstown", "Southampton"),
         main="Embarked (port of embarkation)", col="sienna")
 #str(df.train$Embarked)
-install.packages("vcd")
 
 #travelling class for mosaic plot
 mosaicplot(df.train$Pclass ~ df.train$Survived, 
@@ -78,9 +87,7 @@ mosaicplot(df.train$Embarked ~ df.train$Survived,
            main="Passenger Fate by Port of Embarkation",
            shade=FALSE, color=TRUE, xlab="Embarked", ylab="Survived")
 
-install.packages("corrgram")
-
-require(corrgram)
+#install.packages("corrgram")
 corrgram.data <- df.train
 ## change features of factor type to numeric type for inclusion on correlogram
 corrgram.data$Survived <- as.numeric(corrgram.data$Survived)
@@ -123,10 +130,11 @@ unique(df.train$Title)
 
 #To identify the titles which have at least one record with an age missing, I'll use the bystats function from the Hmisc package.
 
-install.packages("Hmisc")
+#install.packages("Hmisc")
+#require(Hmisc)
 
 options(digits=4)
-require(Hmisc)
+
 bystats(df.train$Age, df.train$Title, 
         fun=function(x)c(Mean=mean(x),Median=median(x)))
 
@@ -210,9 +218,6 @@ summary(df.train$Title)
 
 #All of the work done designing the new Title column can be considered a part of feature engineering. The other features I chose to add are generated using custom function featureEngrg, which can be applied to both the training data in df.train and the Kaggle-provided test data in df.infer.
 
-require(plyr)     # for the revalue function 
-require(stringr)  # for the str_sub function
-
 ## test a character as an EVEN single digit
 isEven <- function(x) x %in% c("0","2","4","6","8") 
 ## test a character as an ODD single digit
@@ -274,6 +279,7 @@ df.train.munged <- df.train[train.keeps]
   
 ## split training data into train batch and test batch
 set.seed(23)
+
 training.rows <- createDataPartition(df.train.munged$Fate, p = 0.8, list = FALSE)
 train.batch <- df.train.munged[training.rows, ]
 test.batch <- df.train.munged[-training.rows, ]
@@ -315,7 +321,7 @@ cv.ctrl <- trainControl(method = "repeatedcv", repeats = 3,
                         summaryFunction = twoClassSummary,
                         classProbs = TRUE)
 
-require(pROC)
+
 #Below is the train function call using the same formula (sans Fare) that we recently passed through glm function. I use the metric argument to tell train to optimize the model by maximizing the area under the ROC curve (AUC). summary(), another extractor function, is called to generate regression coefficients with standard errors and a z-test, plus the residual deviance metric we were watching earlier.
 set.seed(35)
 glm.tune.1 <- train(Fate ~ Sex + Class + Age + Family + Embarked,
