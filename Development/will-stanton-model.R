ba42371842d128c1dacdb5ee4df0436bc620e433
kevinkr <kevinkr@yahoo.com>
2016-07-12 13:34:56 -0500

    New model, will stanton

---
+++ Development/will-stanton-model.R
@@ -0,0 +1,41 @@
+#model based on Will Stanton
+library(caret)
+library(randomForest)
+
+
+readData <- function(path.name, file.name, column.types, missing.types) {
+  read.csv( url( paste(path.name, file.name, sep="") ), 
+            colClasses=column.types,
+            na.strings=missing.types )
+}
+
+Titanic.path <- "https://raw.githubusercontent.com/kevinkr/kaggle-titanic/master/Data/"
+train.data.file <- "train.csv"
+test.data.file <- "test.csv"
+missing.types <- c("NA", "")
+train.column.types <- c('integer',   # PassengerId
+                        'factor',    # Survived 
+                        'factor',    # Pclass
+                        'character', # Name
+                        'factor',    # Sex
+                        'numeric',   # Age
+                        'integer',   # SibSp
+                        'integer',   # Parch
+                        'character', # Ticket
+                        'numeric',   # Fare
+                        'character', # Cabin
+                        'factor'     # Embarked
+)
+test.column.types <- train.column.types[-2]     # # no Survived column in test.csv
+
+train.raw <- readData(Titanic.path, train.data.file, 
+                      train.column.types, missing.types)
+trainSet <- train.raw
+
+test.raw <- readData(Titanic.path, test.data.file, 
+                     test.column.types, missing.types)
+testSet <- test.raw
+
+head(trainSet)
+
+head(testSet)
