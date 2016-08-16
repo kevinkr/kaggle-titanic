0b9fa469095c78639183dbba4cc19bc70f087d16
kevinkr <kevinkr@yahoo.com>
2016-08-16 15:03:59 -0500

    mods

--- Development/libery-and-data-kaggle-titanic.R
+++ Development/libery-and-data-kaggle-titanic.R
@@ -32,12 +32,13 @@ barplot(Parch.prop, main = "Survival Rates by Parch", xlab = "Parch", ylab = "Pe
 Embarked.prop <- prop.table(table(titanic_train$Survived, titanic_train$Embarked),2)
 barplot(Embarked.prop, main = "Survival Rates by Embarked", xlab = "Embarked", ylab = "Percent Survived or Perished", col = c("red", "black"))
 
-Age.bins <- cut(titanic_train$Age, c(0, 10,20,30,40,50,60,70,80,90,100))
+Age.bins <- cut(titanic_train$Age, c(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50,60,70,80,90,100))
 Age.prop <- prop.table(table(titanic_train$Survived, Age.bins),2)
 barplot(Age.prop, main = "Survival Rates by Age", xlab = "Age", ylab = "Percent Survived or Perished", col = c("red", "black"), legend = row.names(Age.prop))
 
 #Data Prep
-titanic_test$Survived <- NA  # Add Survived column to test set so we can combine train and test sets
+# Add Survived column to test set so we can combine train and test sets
+titanic_test$Survived <- NA  
 full_data <- rbind(titanic_train, titanic_test)
 str(full_data)
 
@@ -53,14 +54,18 @@ apply(full_data, 2, FUN = function(x) sum(is.na(x))) # count number of NAs
 #And this code counts the number of blanks using the same method.
 apply(full_data, 2, FUN = function(x) sum(x == ""))  # count number of blanks
 
-#Now we want to clean up some of those NAs. You may notice that we have a lot of missing ages. I’ll touch on those later in this post. For the one missing fare value, I use the median value of all fares within the passenger class that the passenger with the missing fare is in.
+#Now we want to clean up some of those NAs. You may notice that we have a lot of 
+#missing ages. I’ll touch on those later in this post. For the one missing fare 
+#value, I use the median value of all fares within the passenger class that the 
+#passenger with the missing fare is in.
 aggregate(Fare ~ Pclass, data = full_data, FUN = "median")
 
 full_data$Pclass[which(is.na(full_data$Fare))]
 #So, the passenger is in class 3. I therefore assign a value of $8.05 to that passenger’s fare.
 full_data$Fare[which(is.na(full_data$Fare))] <- 8.05
 
-#We also have passengers who have a fare of $0. We impute these passengers’ fares using the median fare of the Pclass as well.
+#We also have passengers who have a fare of $0. We impute these passengers’ 
+#fares using the median fare of the Pclass as well.
 length(which(full_data$Fare == 0)) # How many passengers have a fare of $0?
 
 # assign median fare of respective Pclass to passengers who have a $0 fare.
@@ -69,7 +74,9 @@ full_data$Fare[which(full_data$Fare == 0)] <-
     ifelse(full_data$Pclass[which(full_data$Fare == 0)] == 2, 15.0458, 
     8.05)) 
 
-#Finally, we have two records with missing information for the port of embarkation. I filter the data to find out which records these are, then I determine if one port is more common than the rest.
+#Finally, we have two records with missing information for the port of 
+#embarkation. I filter the data to find out which records these are, then 
+#I determine if one port is more common than the rest.
 full_data[which(full_data$Embarked == ""), ]
 
 prop.table(table(full_data$Embarked))
@@ -107,7 +114,11 @@ full_data$Title <- changeTitles(full_data, c("Mlle", "Mme"), "Miss")
 # We want to change Title from character to factor
 full_data$Title <- as.factor(full_data$Title) 
 
-#Now we return to those missing ages. The reason we didn’t worry about them earlier is because I wanted to use the new title feature to help us impute the ages. Many of the titles suggest a certain age group. For example, “master” was used to refer to a young male. We’ll use a decision tree (rpart package) to estimate the missing ages and then update the Age column with the new ages.
+#Now we return to those missing ages. The reason we didn’t worry about them earlier 
+#is because I wanted to use the new title feature to help us impute the ages. 
+#Many of the titles suggest a certain age group. For example, “master” was used 
+#to refer to a young male. We’ll use a decision tree (rpart package) to estimate 
+#the missing ages and then update the Age column with the new ages.
 
 estimated_age <- rpart(Age ~ Pclass  + Sex + SibSp + Parch + Fare + Embarked + Title, 
                        data = full_data[!is.na(full_data$Age), ], method = "anova")
@@ -124,7 +135,8 @@ barplot(Family.prop, main = "Survival Rates by Family Size",
         xlab = "Family Size", ylab = "Percent Survived or Perished", 
         xlim = c(0,12), , col = c("red", "black"), legend = rownames(Family.prop))
 
-#Of course, the larger families were less common, which we can see by counting the number of passengers that belonged to each family size grouping.
+#Of course, the larger families were less common, which we can see by counting the 
+#number of passengers that belonged to each family size grouping.
 barplot(table(full_data$Family), xlab = "Family Size", ylab = "Count of Passengers", main = "Count of Passengers by Family Size", ylim = c(0,800))
 
 #Now we’re ready to start building some models. We’ll first want to split our full_data back into the train and test sets.
@@ -142,7 +154,12 @@ logistic.model <- glm(Survived ~ Pclass + Sex + Age + SibSp + Parch + Family + F
 logistic.roc <- roc(titanic_train$Survived, predict(logistic.model, newdata = titanic_train, type = "response"))
 plot.roc(logistic.roc, print.auc = TRUE)
 
-#Our AUC on our train set is 0.876, which is not bad at all. Let’s generate the predictions for the test data and create a CSV to submit to Kaggle for scoring. Notice that I use a cutoff probability value of 0.5 to classify whether a passenger survives or not. This value is a parameter that we can adjust to improve our fit, but I won’t get into that in this post. A cutoff of 0.5 is oftentimes the default value used.
+#Our AUC on our train set is 0.876, which is not bad at all. Let’s generate the 
+#predictions for the test data and create a CSV to submit to Kaggle for scoring. 
+#Notice that I use a cutoff probability value of 0.5 to classify whether a 
+#passenger survives or not. This value is a parameter that we can adjust to 
+#improve our fit, but I won’t get into that in this post. 
+#A cutoff of 0.5 is oftentimes the default value used.
 logistic.test <- predict(logistic.model, newdata = titanic_test, type = "response")
 logistic_submission <- data.frame(PassengerId = titanic_test$PassengerId, Survived = ifelse(logistic.test >= 0.5, 1, 0))
 write.csv(logistic_submission, file = "liberty-and-data-logistic_submission.csv", row.names = FALSE)
@@ -152,12 +169,38 @@ write.csv(logistic_submission, file = "liberty-and-data-logistic_submission.csv"
 #################
 #Generalized Additive Model (GAM)
 ##################
-#The second model choice, and the model for which I originally decided to write this post, is the generalized additive model (GAM). GAMs are both interpretable and flexible, which makes them very useful. I will build a fairly simple boosted GAM using the train function in the caret package.
-gam.model <- train(Survived ~ Pclass + Sex + Age + SibSp + Parch + Family + Fare + Embarked + Title, 
+#The second model choice, and the model for which I originally decided to write this 
+#post, is the generalized additive model (GAM). GAMs are both interpretable and 
+#flexible, which makes them very useful. I will build a fairly simple boosted GAM 
+#using the train function in the caret package.
+gbm.model <- train(Survived ~ Pclass + Sex + Age + SibSp + Parch + Family + Fare + Embarked + Title, 
                    data = titanic_train, 
-                   method = "gamboost", 
+                   method = "gbm", 
                    preProc = c("center", "scale"))
 
 #To get a sense of the accuracy, we can look at our model as well as the confusion matrix and statistics.
-gam.model
+gbm.model
+
+gbm.test <- predict(gbm.model, newdata = titanic_test, type = "raw")
+gbm_submission <- data.frame(PassengerId = titanic_test$PassengerId, Survived = ifelse(logistic.test >= 0.5, 1, 0))
+write.csv(gbm_submission, file = "liberty-and-data-gbm_submission.csv", row.names = FALSE)
+#0.77990 with gbm
+
+
+
+###tree 
+library(rpart)
+library("rpart.plot")
+rpart1 <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + 
+                Family + Fare  + Title, 
+                data = titanic_train,
+                method="class",
+                control = rpart.control(xval = 50, minbucket = 1, cp = 0) )
+rpart1
+rpart.plot(rpart1)
+
+rpart1.test <- predict(rpart1, newdata = titanic_test)
+rpart1_submission <- data.frame(PassengerId = titanic_test$PassengerId, Survived = ifelse(logistic.test >= 0.5, 1, 0))
+write.csv(rpart1_submission, file = "liberty-and-data-rpart2_submission.csv", row.names = FALSE)
+#0.77990
 
